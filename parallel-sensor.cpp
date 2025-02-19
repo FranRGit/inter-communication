8bfa78b56f6cb1e71da411f9dc69dfb24de52c8d
FranRGit <148667383+FranRGit@users.noreply.github.com>
2025-02-19 10:01:16 -0500

    first commit

--- parallel-sensor.cpp
+++ parallel-sensor.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <random>
 #include <chrono>
+#include <algorithm> 
 
 const int NUM_SENSORES = 5;
 std::mutex mtx_datos;
@@ -14,7 +15,7 @@ std::vector<int> datos_sensores;
 
 void sensor(int id) {
     std::this_thread::sleep_for(std::chrono::milliseconds(500 * id)); 
-    std::cout << "Sensor " << id << " está listo.\n";
+    std::cout << "Sensor " << id << " esta listo.\n";
 
     barrera.arrive_and_wait(); 
 
@@ -28,23 +29,28 @@ void sensor(int id) {
         datos_sensores.push_back(medicion);
     }
 
-    std::cout << "Sensor " << id << " registró el valor: " << medicion << "\n";
+    std::cout << "Sensor " << id << " registro un valor: " << medicion << "\n";
 }
 
 void ordenarDatos() {
+    std::this_thread::sleep_for(std::chrono::seconds(4));
     { 
         std::lock_guard<std::mutex> lock(mtx_datos);
         std::sort(datos_sensores.begin(), datos_sensores.end());
     }
 
-    std::cout << "Datos ordenados: ";
+    std::cout << "\nDatos ordenados:\n";
     for (int dato : datos_sensores) {
         std::cout << dato << "\n";
     }
-
-    
 }
 
+void mostrarProgreso() {
+    for (int i = 0; i < 3; ++i) {  
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Pocesando datos...\n";
+    }
+}
 int main() {
     std::vector<std::thread> hilos;
 
@@ -52,12 +58,11 @@ int main() {
         hilos.emplace_back(sensor, i + 1);
     }
 
-
     for (auto &hilo : hilos) {
         hilo.join();
     }
-    
-    std::future<void> reporte = std::async(std::launch::async, ordenarDatos);
 
+    std::future<void> reporte = std::async(std::launch::async, ordenarDatos);
+    mostrarProgreso();
     return 0;
 }
